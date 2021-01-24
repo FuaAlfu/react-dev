e00ae9a897b264603b8dc1784392fd3637f998a0
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-01-24 21:51:51 +0300

    add

--- 002-weather-app/src/components/Data.js
+++ 002-weather-app/src/components/Data.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
-const apiKey = ''; //enter your api-key here...
+const apiKey = 'acd5280c8a981795cd8fbdc9e9e46f3b'; //enter your api-key here...
 
 export const getWeatherData = async (cityname) => {
     try{

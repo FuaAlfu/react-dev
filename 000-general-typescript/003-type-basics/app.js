57c268e01d1c09c82dd16728a62de64953b1448e
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-02-23 11:15:16 +0300

    add

---
+++ 000-general-typescript/003-type-basics/app.js
@@ -0,0 +1,19 @@
+var hi = 'hi';
+//arrays
+var names = ['fua', 'mua', 'smoky'];
+names.push('todd');
+var numbers = [10, 20, 30];
+numbers.push(40);
+var mixed = ['jhon', 60, 'noa', 4];
+mixed.push('num');
+mixed.push(11);
+mixed[0] = 22;
+// objects
+var hold = {
+    name: 'gold',
+    price: 23.8,
+    count: 1
+};
+hold.name = 'silver';
+hold.price = 12.6;
+hold.count = 2;
---
+++ 000-general-typescript/003-type-basics/app.ts
@@ -0,0 +1,24 @@
+let hi = 'hi';
+
+//arrays
+let names = ['fua', 'mua' ,'smoky']
+names.push('todd');
+
+let numbers = [10,20,30]
+numbers.push(40);
+
+let mixed = ['jhon', 60,'noa', 4]
+mixed.push('num');
+mixed.push(11);
+mixed[0] = 22;
+
+// objects
+let hold = {
+    name: 'gold',
+    price: 23.8,
+    count: 1
+};
+
+hold.name = 'silver';
+hold.price = 12.6;
+hold.count = 2;

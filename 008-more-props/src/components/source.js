1de961c912612bd06bcc46686c438b54a6a50f9b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-09 23:26:08 +0300

    add

--- 008-more-props/src/components/source.js
+++ 008-more-props/src/components/source.js
@@ -1,20 +1,39 @@
 const data = [
     {id:1, name: 'foo', age: 42},
     {id:2, name: 'hoo', age: 53},
-    {id:3, name: 'goo', age: 64},
+    {id:3, name: 'goo', age: 64, credit: 'your credit: 9999999.00'},
 ]
 
 function List(){
     return(
         <section>
             {data.map((item) =>{
-                return <Person key={item.id}></Person>
+               // return <Person key={item.id} name={item.name}></Person> //old but gold
+                return <Person key={item.id} person={item}></Person> //pass entire object
+              // return <Person key={item.id} {...item}></Person>  //use spread operator
             })}
         </section>
     )
 }
 
+//for spread
+// const Person = ({name,age}) =>{
+//     return <h1>{name} - {age}</h1>
+// }
+
 const Person = (props) =>{
     console.log(props);
-    return <h2> a person</h2>
+    return (
+        <div>
+            <h1>Hi there !</h1>
+            {/*<h2>{props.name}</h2>*/}
+            <h2>{props.person.name} - {props.person.credit}</h2>
+            <h1>your age</h1>
+            <h2>{props.person.age}</h2>
+            <p>---</p>
+        </div>
+        
+    )
 }
+
+export default List

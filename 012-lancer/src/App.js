db615f330669e48aa824fc3eacefd382a292b8b5
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-21 15:31:35 +0300

    add

---
+++ 012-lancer/src/App.js
@@ -0,0 +1,11 @@
+import './components/styles/app.scss';
+import { Header } from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

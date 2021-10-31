97caa79b46143be73527e24e77f2a22210767c31
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-10-31 13:02:05 +0300

    add

---
+++ 014-game-memory/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Magic Match</h1>
+     <button>Start New Game</button>
+    </div>
+  );
+}
+
+export default App;

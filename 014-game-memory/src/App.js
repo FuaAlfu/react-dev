2cd7680af195a64e5ed3da8fd7e65304f1418fcd
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-11-02 14:26:00 +0300

    add

--- 014-game-memory/src/App.js
+++ 014-game-memory/src/App.js
@@ -27,6 +27,18 @@ function App() {
     <div className="App">
      <h1>Magic Match</h1>
      <button onClick={shuffleCards}>Start New Game</button>
+
+     {/* our game grid */}
+     <div className="card-grid">
+       {cards.map(card => (
+         <div className="card" key={card.id}>
+           <div>
+             <img className="front" src={card.src} alt="card front" />
+             <img className="back" src="/img/cover.png" alt="card back"/>
+           </div>
+         </div>
+       ))}
+     </div>
     </div>
   );
 }

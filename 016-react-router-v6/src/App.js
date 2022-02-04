120111aec86c256d0345332ac5f2791edf890123
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-02-04 13:29:07 +0300

    add routes

--- 016-react-router-v6/src/App.js
+++ 016-react-router-v6/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route,Routes } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
@@ -8,6 +9,9 @@ import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 
 function App() {
+  /**
+   * including the old version 
+   */
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,18 +21,32 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
+        {/* <Switch> */}
+        <Routes>
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route path="/" component={Home} /> */}
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about">
             <About />
-          </Route>
-          <Route path="/products/:id">
+          </Route> */}
+           <Route path="/about" element={<About />} />
+          {/* <Route path="/products/:id">
             <ProductDetails />
-          </Route>
-          <Route path="/products">
+          </Route> */}
+          <Route path="/products/:id" element={<ProductDetails />} />
+          {/* <Route path="/products">
             <Products />
-          </Route>
-        </Switch>
+          </Route> */}
+          <Route path="/products" element={ <Products />} />
+          {/* we could inline some jsx template inside the element props, insted referencing these other droop in page components */}
+          {/* example: look at test route */}
+          <Route path="/test" element={(
+               <div>
+                    <h2>test page..</h2>
+               </div>
+          )}/>
+        {/* </Switch> */}
+        </Routes>
       </BrowserRouter>
     </div>
   )
